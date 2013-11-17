52b1582dcabe8002adef4510b6dbe36afa975ebb
Sungwon <Kim.sw.engineer@gmail.com>
2013-11-17 23:44:13 +0900

    UserProfile

--- StickyOnPage/src/com/inha/stickyonpage/MemoAdapter.java
+++ StickyOnPage/src/com/inha/stickyonpage/MemoAdapter.java
@@ -1,5 +1,7 @@
 package com.inha.stickyonpage;
 
+import java.util.List;
+
 import android.content.Context;
 import android.graphics.Color;
 import android.view.Gravity;
@@ -9,26 +11,30 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import com.inha.stickyonpage.db.Sticky;
 
-public class MemoAdapter extends ArrayAdapter<String> {
+
+public class MemoAdapter extends ArrayAdapter<Sticky> {
 	
 	LayoutInflater inflater;
-	String[] array;
+	List<Sticky> stickies;
+	int resource;
 	
-	public MemoAdapter(Context context, int resource, String[] objects) {
-		super(context, resource, objects);
+	public MemoAdapter(Context context, int resource, List<Sticky> stickies) {
+		super(context, resource, stickies);
 		// TODO Auto-generated constructor stub
 		inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		array = objects;
+		this.stickies = stickies;
+		this.resource = resource;
 	}
-	
+
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		// TODO Auto-generated method stub
 		TextView tv; 
 		
 		if(convertView == null)
-			convertView = inflater.inflate(R.layout.memo, parent, false);
+			convertView = inflater.inflate(resource, parent, false);
 		
 		tv = (TextView)convertView.findViewById(R.id.memo);
 		
@@ -40,16 +46,17 @@ public class MemoAdapter extends ArrayAdapter<String> {
 		else if(position % 4 == 1 || position % 4 == 2)
 		{
 			convertView.setBackgroundColor(Color.parseColor("#F9FFB8"));
-			tv.setText(array[position]);
+			tv.setText(stickies.get(position).getMemo());
 			tv.setTextSize(15);
 			tv.setGravity(Gravity.TOP);
 		}else{
 			convertView.setBackgroundColor(Color.parseColor("#C3FFB8"));
-			tv.setText(array[position]);
+			tv.setText(stickies.get(position).getMemo());
 			tv.setTextSize(15);
 			tv.setGravity(Gravity.TOP);
 		}
 		
 		return convertView;
 	}
+
 }
